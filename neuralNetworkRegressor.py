b4b4ca36e8ab8b9521113fd08099e9608dcf44cc
Ross Kohler <rossdkohler@gmail.com>
2018-12-26 13:10:04 +0200

    attempting to fix memory issues

--- neuralNetworkRegressor.py
+++ neuralNetworkRegressor.py
@@ -9,10 +9,52 @@ import gc
 
 gc.enable()
 
+data_test = "./data/test_V2.csv"
+data_train = "./data/train_V2.csv"
 
-csvrows = 10000000
+
+csvrows = 1000
 
 features = []
+
+# Memory saving function credit to https://www.kaggle.com/gemartin/load-data-reduce-memory-usage
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.
+    """
+    #start_mem = df.memory_usage().sum() / 1024**2
+    #print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    #end_mem = df.memory_usage().sum() / 1024**2
+    #print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    #print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
+
+
 
 def addGroupSize(df):
     groupSize=df.groupby(['matchId','groupId']).size().reset_index(name='group_size')
@@ -69,7 +111,8 @@ def averageWinPlacePercForGroup(df):
 
 
 print("Reading training data...")
-df_train = pd.read_csv("./data/train_V2.csv",nrows=csvrows)
+df_train = pd.read_csv(data_train,nrows=csvrows)
+df_train = reduce_mem_usage(df_train)
 
 df_train = df_train[df_train.maxPlace>1]
 
@@ -84,13 +127,9 @@ features = [e for e in features if e not in {"winPlacePerc",
     'matchType'}]
 
 
-print("Reading test data...")
-df_test = pd.read_csv("./data/test_V2.csv",nrows=csvrows)
-
 df_train.replace([np.inf, -np.inf], np.nan)
 
 print("Looking for NA values in Dataframes...")
-print("Number of rows with NA in DF_TEST:",len(df_test[df_test.isnull().any(axis=1)]))
 print("Number of rows with NA in DF_TRAIN:",len(df_train[df_train.isnull().any(axis=1)]))
 
 
@@ -112,6 +151,9 @@ df_train = matchSize(df_train)
 
 print("Preparing data for training...")
 Y_train = df_train['winPlacePerc']
+Y_train = np.array(Y_train, dtype=np.float64);
+
+
 X = df_train.copy().drop(['winPlacePerc'],axis=1)
 
 
@@ -121,12 +163,10 @@ del df_train
 gc.collect()
 
 X_train = X.drop(['Id','groupId','matchId','matchType'],axis=1)
-
+X_train = np.array(X_train, dtype=np.float64);
 del X
 gc.collect()
 
-
-print("Number of features: %s"%len(X_train.columns))
 
 
 scaler = preprocessing.MinMaxScaler(feature_range=(-1,1),copy=False).fit(X_train)
@@ -143,6 +183,18 @@ model.fit(X_train,Y_train)
 
 print("Regression Report: %s:" % (model.score(X_train,Y_train)))
 
+del X_train
+del Y_train
+
+gc.collect()
+
+print("Reading test data...")
+df_test = pd.read_csv(data_test,nrows=csvrows)
+df_test = reduce_mem_usage(df_test)
+
+print("Looking for NA values in Dataframes...")
+print("Number of rows with NA in DF_TEST:",len(df_test[df_test.isnull().any(axis=1)]))
+
 print("Preparing test data for predictions...")
 df_test = addGroupSize(df_test)
 df_test = total_distance(df_test)
@@ -155,6 +207,9 @@ df_test = matchSize(df_test)
 
 print("Preparing Test Data for predictions...")
 X_test = df_test.drop(['Id','groupId','matchId','matchType'],axis=1)
+X_test = np.array(X_test, dtype=np.float64);
+
+
 scaler.transform(X_test)
 
 print("Making Predictions...")
